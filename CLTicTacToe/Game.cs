57e0cdaa4d32f9fc304ede4ece6579eb9711d82f
Michael Robinson <miggello1@gmail.com>
2018-10-15 16:08:13 -0400

    Created a skeleton control flow, and started fleshing out the 'game' class to store information about each game.

---
+++ CLTicTacToe/Game.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLTicTacToe
+{
+    class Game
+    {
+        private string _name;
+        private result _gameResult;
+        private int _playerNumber;
+
+        public Game()
+        {
+            _name = "Default";
+            _gameResult = result.Incomplete;
+            _playerNumber = assignPlayer();
+        }
+
+        private int assignPlayer()
+        {
+            Random rand = new Random();
+            if (rand.NextDouble() > 0.5)
+            {
+                return 1;
+            }
+            else
+            {
+                return 2;
+            }
+        }
+
+        private enum result
+        {
+            Incomplete = 1,
+            Draw,
+            Win,
+            Loss
+        }
+
+        protected List<int> Moves { get; set; }
+
+        //TODO Add error handling for invalid names.
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        //TODO create make move method
+        public void makeMove()
+        {
+            if (Moves.Count() == 9)
+            {
+                _gameResult = evaluateGame();
+            }
+        }
+
+        //Evaluate Game for Winner
+        private result evaluateGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        //TODO create draw board method
+        public void drawBoard()
+        {
+
+        }
+    }
+}
--- CLTicTacToe/Program.cs
+++ CLTicTacToe/Program.cs
@@ -8,14 +8,40 @@ namespace CLTicTacToe
 {
     class Program
     {
+        /*Planned Features:
+            1. Play Tic-Tac-Toe against the computer.
+            2. Draw game as it progresses using a simple command line GUI.
+            3. Computer should have multiple difficulty settings (easy - normal - impossible).
+            4. Store game data in a basic user profile.
+            5. Calculate statistics based on user profile and game data.
+        */
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string response;
+            Console.WriteLine("Welcome to Command Line Tic Tac Toe! Would you like to play a game? (y/n)\n");
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            do
+            {
+                response = Console.ReadLine();
+                if (new[] { "Y", "y", "Yes", "yes" }.Contains(response))
+                {
+                    //Start Play Menu
+                    Console.WriteLine("You played a game! (Not Yet Implemented)");
+                    Console.WriteLine("Would you like to play another?");
+                }
+                else if (new[] { "N", "n", "No", "no" }.Contains(response))
+                {
+                    //exit application
+                    break;
+                }
+                else
+                {
+                    //invalid response
+                    Console.WriteLine("Invalid Response, please enter 'y' for yes, or 'n' for no.");
+                }
+            }
+            while (true);
+            
         }
     }
 }
